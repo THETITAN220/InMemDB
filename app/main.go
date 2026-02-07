4a18d139b374ec572577d9b73d3f8e12303e991e
THETITAN220 <raunakrose15@gmail.com>
2026-02-08 00:04:17 +0530

    Dont remember the change its been a while

--- app/main.go
+++ app/main.go
@@ -29,23 +29,38 @@ func main() {
 		}
 
 		go handleConnection(conn)
-
 	}
-
 }
 
 func handleConnection(conn net.Conn) {
 
 	defer conn.Close()
 
-	fmt.Printf("Client conncted from %s\n", conn.RemoteAddr())
+	fmt.Printf("Client conncted to the port %s\n", conn.RemoteAddr())
 
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
-		text := scanner.Text()
-		if strings.TrimSpace(text) == "PING" {
-			conn.Write([]byte("+PONG\r\n"))
+		text := strings.TrimSpace(scanner.Text())
+		if text == "PING" {
+			_, err := conn.Write([]byte("+PONG\r\n"))
+			if err != nil {
+				fmt.Println("Error writing PONG response: ", err.Error())
+				break
+			}
+		} else {
+			errorMessage := fmt.Sprintf(" Error invalid command: %s\r\n ", text)
+			_, err := conn.Write([]byte(errorMessage))
+			if err != nil {
+				fmt.Println("Error writing error response: ", err.Error())
+				break
+			}
 		}
-		conn.Write([]byte(" Error invalid command " + text + "\r\n"))
+
+		if err := scanner.Err(); err != nil && err.Error() != "EOF" {
+			fmt.Printf("Error reading from connection %s: %v \n", conn.RemoteAddr(), err)
+		}
+
 	}
+
+	fmt.Printf("Client disconnected from %s \n", conn.RemoteAddr())
 }
